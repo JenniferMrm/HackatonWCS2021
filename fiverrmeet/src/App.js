e55eb90a51e1a57d616cc363f7d5d29c651deca7
Anaïs Jarnac <ajarnac@gmail.com>
2021-06-28 17:54:37 +0200

    accueil

--- fiverrmeet/src/App.js
+++ fiverrmeet/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Accueil from "./view/accueil/Accueil";
+
 
 function App(props) {
-  return <div></div>;
+  return <div >
+  <Accueil />
+  </div>;
 }
 
 export default App;
--- fiverrmeet/src/view/accueil/Accueil.jsx
+++ fiverrmeet/src/view/accueil/Accueil.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./accueil.css";
 
 function Accueil(props) {
-  return <div></div>;
+  
+
+
+  return <div>
+    <img className = "logo"
+    alt="logoaccueil"
+    src="./assets/Logo.png"/>
+  </div>;
 }
 
 export default Accueil;
