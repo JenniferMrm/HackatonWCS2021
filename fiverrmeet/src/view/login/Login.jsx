275140efec74150cc081c87d6ba513c64b4732ab
Anaïs Jarnac <ajarnac@gmail.com>
2021-06-29 11:28:19 +0200

    login

--- fiverrmeet/src/view/login/Login.jsx
+++ fiverrmeet/src/view/login/Login.jsx
@@ -1,29 +1,69 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import SendIcon from "@material-ui/icons/Send";
 
 const useStyles = makeStyles((theme) => ({
   button: {
-    margin: theme.spacing(1),
+    backgroundColor: "white",
+    color: "black",
+    width: "70%",
+    height: "40px",
+    borderRadius: "30px",
+    margin: "20px",
+    fontFamily: "Poppins",
   },
+
+  back: {
+    height: "815px",
+  },
+
+  background: {
+    backgroundImage: "url('/assets/fondco.png')",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    bottom: "0",
+    maxWidth: "100%",
+    height: "100%",
+  },
+  icon: {
+    fontSize: "20px",
+    color: "white",
+    backgroundColor: "#1FBF73",
+    borderRadius: "50%",
+    padding: "10px",
+   
+  },
+
+  contain: {
+    position: "relative",
+    top: "200px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  
+  },
+  icon2: {
+    marginLeft: "20px",
+  }
 }));
 
 export default function IconLabelButtons() {
   const classes = useStyles();
 
   return (
-    <div>
-      <Button
-        variant="contained"
-        color="primary"
-        size="small"
-        className={classes.button}
-        startIcon={<LockOpenIcon />}
-      >
-        Se connecter avec Fiverr
-      </Button>
-
+    <div className={classes.background}>
+      <div className={classes.back}>
+        <div className={classes.contain}>
+          <LockOpenIcon className={classes.icon} />
+          <Button size="small" className={classes.button}>
+            Connected with FIVERR
+            <SendIcon className={classes.icon2}/>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
