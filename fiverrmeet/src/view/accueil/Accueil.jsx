0d4e5072cfaa7150490373272dfd4c79de040973
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2021-06-29 17:16:14 +0200

    message view

--- fiverrmeet/src/view/accueil/Accueil.jsx
+++ fiverrmeet/src/view/accueil/Accueil.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-
 
 function Accueil(props) {
-  const history = useHistory()
- setTimeout(() =>{
-   history.push('/Login')
- },4990)
-
-
-
-  return <div>
-    <img className = "logo"
-    id="logo-accueil" 
-    alt="logoaccueil"
-    src="./assets/Logo.png"/>
-  </div>;
+  return <div></div>;
 }
 
 export default Accueil;
--- fiverrmeet/src/view/login/Login.jsx
+++ fiverrmeet/src/view/login/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import LockOpenIcon from "@material-ui/icons/LockOpen";
 import SendIcon from "@material-ui/icons/Send";
 
 const useStyles = makeStyles((theme) => ({
